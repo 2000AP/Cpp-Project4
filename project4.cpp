76b49c1a9f9b2454257fa6515415e44e8372e1d1
2000AP <88373411+2000AP@users.noreply.github.com>
2021-09-02 12:02:12 +0530

    Project based on array using pointer
    In this project user has to put any 5 integer elements, and same will be printed at output.

---
+++ project4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    cout << "Enter elements: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    int *p;
+    p = arr;
+    cout << "You entered: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *p << endl;
+        p++;
+    }
+    return 0;
+}
